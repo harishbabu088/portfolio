78218576ddc354b2b57ed0b3f26161c54fece050
Harish,Achanta <harishbabu088@gmail.com>
2020-07-18 21:33:02 -0400

    angular1assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
+import { Tab1Component } from './tabs/tab1/tab1.component';
+import { Tab2Component } from './tabs/tab2/tab2.component';
+import { Tab3Component } from './tabs/tab3/tab3.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'/tab1', component:'Tab1Component'}
+  { path: '', redirectTo: '/tabs/tab1', pathMatch: 'full' },
+  {
+    path: 'tabs',
+    component: TabsComponent,
+    children: [
+      {
+        path: 'tab1',
+        component: Tab1Component,
+      },
+      {
+        path: 'tab2',
+        component: Tab2Component,
+      },
+      {
+        path: 'tab3',
+        component: Tab3Component,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { Tab1Component } from './tabs/tab1/tab1.component';
+import { Tab2Component } from './tabs/tab2/tab2.component';
+import { Tab3Component } from './tabs/tab3/tab3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    Tab1Component,
+    Tab2Component,
+    Tab3Component
   ],
   imports: [
     BrowserModule,
